d56255ad1924d2db2ffed7d84323ab496f333923
master <58977579+alexmasterblack@users.noreply.github.com>
2020-10-24 19:47:12 +1000

    Добавьте файлы проекта.

---
+++ ComplexNumber_Test/ComplexNumber_Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ComplexNumberTest
+{
+	TEST_CLASS(ComplexNumberTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
