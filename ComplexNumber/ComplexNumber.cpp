e622d4c6a91591e95021cf7a42ed24e6357df0bb
master <58977579+alexmasterblack@users.noreply.github.com>
2020-10-25 22:54:43 +1000

    Rational

--- ComplexNumber/ComplexNumber.cpp
+++ ComplexNumber/ComplexNumber.cpp
@@ -1,20 +1,180 @@
-﻿// ComplexNumber.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath> 
+#include <string>
+#include <sstream>   
 
-#include <iostream>
+using namespace std;
+
+class RationalNumber {
+    int64_t numerator = 0;
+    int64_t denominator = 1;
+public:
+    RationalNumber() {}
+    RationalNumber(double number) {
+        if (number == static_cast<int64_t>(number)) {
+            numerator = static_cast<int64_t>(number);
+            denominator = 1;
+        }
+        else {
+            ostringstream str;
+            str << number;
+            string decimal = str.str();
+            string after_point = decimal.substr(decimal.find('.') + 1);
+            int lenght = after_point.length();
+
+            for (; lenght > 0; lenght--) {
+                number *= 10;
+                denominator *= 10;
+            }
+            numerator = number;
+            Reduction();
+        }
+    }
+    RationalNumber(int64_t numerator, int64_t denominator) {
+        if (denominator != 0) {
+            this->numerator = numerator;
+            this->denominator = denominator;
+        }
+        if (this->denominator <= 0) {
+            this->denominator = -this->denominator;
+            this->numerator = -this->numerator;
+        }
+        if (this->numerator != 0) {
+            Reduction();
+        }
+        else {
+            this->denominator = 1;
+        }
+    }
+    int64_t GreatestCommonFactor(int64_t number_one, int64_t number_two) {
+        if (number_one == number_two) {
+            return number_one;
+        }
+        if (number_one > number_two) {
+            int64_t support = number_one;
+            number_one = number_two;
+            number_two = support;
+        }
+        return GreatestCommonFactor(number_one, number_two - number_one);
+    }
+    void Reduction() {
+        int64_t div = GreatestCommonFactor(abs(numerator), denominator);
+        numerator /= div;
+        denominator /= div;
+    }
+    RationalNumber& operator=(const RationalNumber& other) {
+        numerator = other.numerator;
+        denominator = other.denominator;
+        return *this;
+    }
+    RationalNumber operator+(const RationalNumber& other) const {
+        int64_t new_num, new_den;
+        if (denominator != other.denominator) {
+            new_num = numerator * other.denominator + other.numerator * denominator;
+            new_den = denominator * other.denominator;
+        }
+        else {
+            new_num = numerator + other.numerator;
+            new_den = denominator;
+        }
+        return RationalNumber(new_num, new_den);
+    }
+    RationalNumber& operator+=(const RationalNumber& other) {
+        *this = *this + other;
+        return *this;
+    }
+    RationalNumber operator-(const RationalNumber& other) const {
+        int64_t new_num, new_den;
+        if (denominator != other.denominator) {
+            new_num = numerator * other.denominator - other.numerator * denominator;
+            new_den = denominator * other.denominator;
+        }
+        else {
+            new_num = numerator - other.numerator;
+            new_den = denominator;
+        }
+        return RationalNumber(new_num, new_den);
+    }
+    RationalNumber& operator-=(const RationalNumber& other) {
+        *this = *this - other;
+        return *this;
+    }
+    RationalNumber operator*(const RationalNumber& other) const {
+        return RationalNumber(numerator * other.numerator, denominator * other.denominator);
+    }
+    RationalNumber& operator*=(const RationalNumber& other) {
+        *this = *this * other;
+        return *this;
+    }
+    RationalNumber operator/(const RationalNumber& other) const  {
+        return RationalNumber(numerator * other.denominator, denominator * other.numerator);
+    }
+    RationalNumber& operator/=(const RationalNumber& other) {
+        *this = *this / other;
+        return *this;
+    }
+    bool operator==(const RationalNumber& other) const {
+        return numerator == other.numerator && denominator == other.denominator;
+    }
+    bool operator!=(const RationalNumber& other) const {
+        return !(*this == other);
+    }
+    RationalNumber operator-() const {
+        return RationalNumber(-numerator, denominator);
+    }
+    int64_t GetNumerator() {
+        return numerator;
+    }
+    int64_t GetDenominator() {
+        return denominator;
+    }
+    string GetDecimal() {
+        return to_string(numerator) + '/' + to_string(denominator);
+    }
+};
+
+class ComplexNumber {
+private:
+    RationalNumber real_num;
+    RationalNumber imag_part;
+public:
+    ComplexNumber() {}
+    explicit ComplexNumber(double real_num, double imag_part) {
+        this->real_num = RationalNumber(real_num);
+        this->imag_part = RationalNumber(imag_part);
+    }
+    ComplexNumber(const ComplexNumber& other) {
+        real_num = other.real_num;
+        imag_part = other.imag_part;
+    }
+    ComplexNumber& operator=(const ComplexNumber& other) {
+        real_num = other.real_num;
+        imag_part = other.imag_part;
+        return *this;
+    }
+    RationalNumber GetReal() {
+        return real_num;
+    }
+    RationalNumber GetImag() {
+        return imag_part;
+    }
+    string GetComplex() {
+        return GetReal().GetDecimal() + " + i " + GetImag().GetDecimal();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    RationalNumber l;
+    cout << l.GetDecimal() << endl;
+
+
+    ComplexNumber c;
+    cout << c.GetComplex() << endl;
+    ComplexNumber a(1.5, 2);
+    cout << a.GetComplex() << endl;
+    ComplexNumber b(1.2, 7);
+    cout << b.GetComplex() << endl;
+    a = b;
+    cout << a.GetComplex() << endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
